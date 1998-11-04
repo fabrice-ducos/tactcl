a32238b14f7ce24cdeb16eb54d750ea23599182b
hylands <hylands>
1998-11-04 22:40:06 +0000

    JDK1.2fcs: defineClass() throws a NoClassDefFound exception, which we now catch (Mo DeJong, Christopher Hylands)

--- src/tcljava/tcl/lang/TclClassLoader.java
+++ src/tcljava/tcl/lang/TclClassLoader.java
@@ -24,7 +24,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id$
+ * RCS: @(#) $Id: TclClassLoader.java,v 1.2 1998/11/03 20:19:16 hylands Exp $
  */
 
 
@@ -196,7 +196,11 @@ throws
      * Define it (parse the class file)
      */
     
-    result = defineClass(className, classData, 0, classData.length);
+    try {
+        result = defineClass(className, classData, 0, classData.length);
+    } catch (NoClassDefFoundError e) {
+	throw new ClassFormatError();
+    }
     if (result == null) {
 	throw new ClassFormatError();
     }
