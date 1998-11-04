945c1c9159792ca8f74ed5abc862490091893f24
hylands <hylands>
1998-11-04 22:37:24 +0000

    Don't load the exec command on demand if we are on a Mac (Mo DeJong)

--- src/jacl/tcl/lang/Interp.java
+++ src/jacl/tcl/lang/Interp.java
@@ -10,7 +10,7 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  * 
- * RCS: @(#) $Id$
+ * RCS: @(#) $Id: Interp.java,v 1.2 1998/11/03 20:06:54 hylands Exp $
  *
  */
 
@@ -653,7 +653,9 @@ createCommands()
     Extension.loadOnDemand(this, "eof",	 	  "tcl.lang.EofCmd");
     Extension.loadOnDemand(this, "eval",	  "tcl.lang.EvalCmd");
     Extension.loadOnDemand(this, "error",	  "tcl.lang.ErrorCmd");
-    Extension.loadOnDemand(this, "exec",	  "tcl.lang.ExecCmd");
+    if (! Util.isMac()) {
+	Extension.loadOnDemand(this, "exec",	  "tcl.lang.ExecCmd");
+    }
     Extension.loadOnDemand(this, "exit",	  "tcl.lang.ExitCmd");
     Extension.loadOnDemand(this, "expr",	  "tcl.lang.ExprCmd");
     Extension.loadOnDemand(this, "fblocked", 	  "tcl.lang.FblockedCmd");
