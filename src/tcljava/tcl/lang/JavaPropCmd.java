b89cd664070a0a0a27b282b1a1d19181e77125fb
dejong <dejong>
1999-05-09 22:23:44 +0000

    fixed comments

--- src/tcljava/tcl/lang/JavaPropCmd.java
+++ src/tcljava/tcl/lang/JavaPropCmd.java
@@ -9,15 +9,13 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  *
- * RCS: @(#) $Id$
+ * RCS: @(#) $Id: JavaPropCmd.java,v 1.2.1.2 1999/02/04 08:44:08 dejong Exp $
  *
  */
 
 package tcl.lang;
 
-/*
- * This class implements the built-in "java::prop" command.
- */
+// This class implements the built-in "java::prop" command.
 
 class JavaPropCmd implements Command {
 
@@ -54,10 +52,8 @@ throws
 	throw new TclException(interp, usage(argv[0]));
     }
 
-    /*
-     * Check the validity of the arguments. N.B., the -noconvert flag
-     * is allowed only in value query.
-     */
+    // Check the validity of the arguments. N.B., the -noconvert flag
+    // is allowed only in value query.
 
     String arg1 = argv[1].toString();
     if ((arg1.length() >= 2) && ("-noconvert".startsWith(arg1))) {
@@ -83,16 +79,12 @@ throws
     }
 
     if (query) {
-	/*
-	 * Query one property.
-	 */
+	// Query one property.
 
 	interp.setResult(JavaInvoke.getProperty(interp, argv[objIndex],
 		argv[objIndex+1], convert));
     } else {
-	/*
-	 * Set one or more properties.
-	 */
+	// Set one or more properties.
 
 	for (int i = objIndex + 1; i < argv.length; i += 2) {
 	    JavaInvoke.setProperty(interp, argv[objIndex], argv[i], argv[i+1]);
