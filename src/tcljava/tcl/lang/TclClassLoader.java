2b34e2a3c81c1751d14b00d2b4fe116c0631dcfe
mo <mo>
1999-08-28 02:43:16 +0000

    added extra resetResult for exception case

--- src/tcljava/tcl/lang/TclClassLoader.java
+++ src/tcljava/tcl/lang/TclClassLoader.java
@@ -24,7 +24,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: TclClassLoader.java,v 1.5 1999/08/03 03:31:08 mo Exp $
+ * RCS: @(#) $Id: TclClassLoader.java,v 1.6 1999/08/09 08:36:19 mo Exp $
  */
 
 
@@ -531,6 +531,7 @@ throws TclException
 	return TclList.getElements(interp,
 	           interp.getVar("env", "TCL_CLASSPATH", TCL.GLOBAL_ONLY));
     } catch (TclException e) {
+	interp.resetResult();
 	return null;
     }
 }
