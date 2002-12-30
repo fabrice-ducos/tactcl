271ff5c2d272ba12e642491c12408d5e02f71112
mdejong <mdejong>
2002-12-30 02:30:54 +0000

    * src/tcljava/tcl/lang/ArrayObject.java (initArray): 	Generate TclException instead of ReflectException. 	* src/tcljava/tcl/lang/JavaDefineClassCmd.java (cmdProc): 	Remove pointless wrapping of Exception in ReflectException. 	* tests/tcljava/ArrayObject.test: Update error message.

--- src/tcljava/tcl/lang/JavaDefineClassCmd.java
+++ src/tcljava/tcl/lang/JavaDefineClassCmd.java
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id$
+ * RCS: @(#) $Id: JavaDefineClassCmd.java,v 1.1.1.1 1998/10/14 21:09:14 cvsadmin Exp $
  */
 
 package tcl.lang;
@@ -54,12 +54,8 @@ throws
     tclClassLoader = new TclClassLoader(interp, null);
     result = tclClassLoader.defineClass(null, classData);
     
-    try {
-	interp.setResult(ReflectObject.newInstance(interp,
-					      Class.class, result));
-    } catch (Exception e) {
-	throw new ReflectException(interp, e);
-    }
+    interp.setResult(ReflectObject.newInstance(interp,
+            Class.class, result));
 }
 
 } // end JavaNewCmd
