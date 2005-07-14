b3ad908515f091645aafc4b3f02aecf2308d0edd
mdejong <mdejong>
2005-07-14 02:04:49 +0000

    * src/jacl/tcl/lang/Expression.java (ExprGetValue): 	Check for a subexpression that has no right hand 	operand and raise a syntax error if found. 	* tests/tcl/expr.test: Check for error condition. 	Thanks go to Jason Lohrey for this bug id and patch. 	[Bug 1071611]

--- src/jacl/tcl/lang/Expression.java
+++ src/jacl/tcl/lang/Expression.java
@@ -8,7 +8,7 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  * 
- * RCS: @(#) $Id: Expression.java,v 1.10 2003/02/04 00:35:41 mdejong Exp $
+ * RCS: @(#) $Id: Expression.java,v 1.11 2005/07/14 00:43:50 mdejong Exp $
  *
  */
 
@@ -599,6 +599,9 @@ class Expression {
 		value2 = ExprGetValue(interp, precTable[operator]);
 	    }
 
+	    if (value2 == null) {
+		SyntaxError(interp);
+	    }
 
 	    if ((m_token < MULT) && (m_token != VALUE)
 		    && (m_token != END) && (m_token != COMMA)
