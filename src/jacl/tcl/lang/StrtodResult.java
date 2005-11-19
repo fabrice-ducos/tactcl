f516179880fd148b6f26ae4bf5b753569ea156aa
mdejong <mdejong>
2005-11-19 01:09:06 +0000

    * src/jacl/tcl/lang/AfterCmd.java (getAfterEvent):         Update strtoul() caller.         * src/jacl/tcl/lang/Expression.java (ExprParseString):         Update strtoul() and strtod() callers.         * src/jacl/tcl/lang/FormatCmd.java (strtoul):         Update callers of FormatCmd.strtoul() and fixup         a number of formatting and StringBuffer issues.         * src/jacl/tcl/lang/ScanCmd.java (cmdProc):         Update strtoul() and strtod() callers and fixup         formatting issues.         * src/jacl/tcl/lang/StringCmd.java (cmdProc):         Update strtoul() and strtod() callers.         * src/jacl/tcl/lang/StrtodResult.java: Fixup comment.         * src/jacl/tcl/lang/Util.java (strtoul, getInt,         strtod, getDouble): Update strtoul() and strtod()         to support parsing a sign indicator from the front         of the string. The previous implementation was buggy         as it saw strings like "- 100" as integers. Also         support a "nan" as a double string.         * tests/tcl/parseExpr.test: Enable test.         * tests/tcl/string.test: Add number parsing test.

--- src/jacl/tcl/lang/StrtodResult.java
+++ src/jacl/tcl/lang/StrtodResult.java
@@ -10,7 +10,7 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  * 
- * RCS: @(#) $Id: StrtodResult.java,v 1.1.1.1 1998/10/14 21:09:21 cvsadmin Exp $
+ * RCS: @(#) $Id: StrtodResult.java,v 1.2 2005/09/30 02:12:17 mdejong Exp $
  *
  */
 
@@ -25,7 +25,7 @@ class StrtodResult {
 /*
  * If the conversion is successful, errno = 0;
  *
- * If the number cannot be converted to a valid unsigned 32-bit integer,
+ * If the number cannot be converted to a valid signed 64-bit double,
  * contains the error code (TCL.DOUBLE_RANGE or TCL.UNVALID_DOUBLE).
  */
 
