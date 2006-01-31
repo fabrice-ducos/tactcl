1699ceb828363d35607fe6dd0523c5b67f463118
mdejong <mdejong>
2006-01-31 00:49:21 +0000

    * docs/TclJava/JavaDefineClassCmd.html:         * src/tcljava/tcl/lang/JavaDefineClassCmd.java:         * src/tcljava/tcl/lang/TclClassLoader.java:         * src/tests/tcljava/javaload/Test3Cmd.java:         * src/tests/tcljava/javaload/Test4Cmd.java:         * tests/tcljava/JavaDefineClassCmd.test:         Add ability to directly pass byte[] object         to the java::defineclass command.

---
+++ src/tests/tcljava/javaload/Test4Cmd.java
@@ -0,0 +1,24 @@
+/*
+ *  Test4.java
+ *
+ * Copyright (c) 1997 Sun Microsystems, Inc.
+ *
+ * See the file "license.terms" for information on usage and
+ * redistribution of this file, and for a DISCLAIMER OF ALL
+ * WARRANTIES.
+ * 
+ * RCS: @(#) $Id: Test4Cmd.java,v 1.1 1999/05/10 04:08:55 dejong Exp $
+ *
+ */
+
+package tests.javaload;
+
+import tcl.lang.*;
+
+public class
+Test4Cmd implements Command {
+    public void cmdProc(Interp interp, TclObject argv[]) throws TclException {
+	interp.setResult("test works");
+    }
+}
+
