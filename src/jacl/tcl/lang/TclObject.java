45a844f23a6ce8bf00c65dee0e79b306a84d3a1c
mdejong <mdejong>
2006-02-24 08:21:42 +0000

    * Makefile.in:         * jtclsh.in:         * tcljava.m4:         * src/aolserver/src/nsjava.c:         * src/jacl/tcl/lang/TclObject.java:         * src/tclblend/tcl/lang/TclObject.java:         * src/tcljava/tcl/lang/TclObjectBase.java:         * src/tjc/tcl/lang/TJCThread.java:         * src/tjc/tjc/library/reload.tcl:         * tests/tjc/compileproc_0.test:         * tests/tjc/compileproc_1.test: Fixup poor spelling.

--- src/jacl/tcl/lang/TclObject.java
+++ src/jacl/tcl/lang/TclObject.java
@@ -7,7 +7,7 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  * 
- * RCS: @(#) $Id: TclObject.java,v 1.2 2006/01/25 03:07:43 mdejong Exp $
+ * RCS: @(#) $Id: TclObject.java,v 1.3 2006/02/14 03:59:52 mdejong Exp $
  *
  */
 
@@ -84,7 +84,7 @@ public final class TclObject extends TclObjectBase {
     /**
      * Return a String that describes TclObject and internal
      * rep type allocations and conversions. The string is
-     * in lines seperated by newlines. The saveObjRecords
+     * in lines separated by newlines. The saveObjRecords
      * needs to be set to true and Jacl recompiled for
      * this method to return a useful value.
      */
