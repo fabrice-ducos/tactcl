936de0b32cfd188c730ab1abc0714d80b74a3587
mdejong <mdejong>
2006-05-22 21:23:35 +0000

    * src/jacl/tcl/lang/Expression.java (ExprParseString,         looksLikeInt):         Update strtod() caller and break out of space         scanning loop as soon as non-space is found.         Optimize looksLikeInt, invoke charAt() only         when needed and use inlined char range.         * src/jacl/tcl/lang/ParseExpr.java:         * src/jacl/tcl/lang/ScanCmd.java:         * src/jacl/tcl/lang/StringCmd.java:         Update strtod() caller.         * src/jacl/tcl/lang/Util.java (strtoul, strtod,         CharAt): Optimize int and double parsing functions,         call charAt() only when needed and avoid invoking         functions when char literal can be tested.         * tests/tcl/expr.test: Add literal tests.         * tests/tclbench/expr.bench: Add runtime test         for case where non-numeric string is         compared to itself over an over.         * tests/tclbench/runbench.tcl:

--- src/jacl/tcl/lang/ScanCmd.java
+++ src/jacl/tcl/lang/ScanCmd.java
@@ -7,7 +7,7 @@
  * redistribution of this file, and for a DISCLAIMER OF ALL
  * WARRANTIES.
  * 
- * RCS: @(#) $Id: ScanCmd.java,v 1.3 2005/09/30 02:12:17 mdejong Exp $
+ * RCS: @(#) $Id: ScanCmd.java,v 1.4 2005/11/19 01:09:06 mdejong Exp $
  *
  */
 
@@ -289,11 +289,11 @@ class ScanCmd implements Command {
 			    if (widthFlag) {
 			        strd = interp.strtodResult;
 			        Util.strtod(new String(scanArr,
-                                          0, width+scanIndex), scanIndex, strd);
+                                          0, width+scanIndex), scanIndex, -1, strd);
 			    } else {
 			        strd = interp.strtodResult;
 			        Util.strtod(new String(scanArr), 
-				        scanIndex, strd);
+				        scanIndex, -1, strd);
 			    }
 			    if (strd.errno != 0) {
 			        scanOK = false;
