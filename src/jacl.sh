5aad858ff7522853e3a27f7711cd10450ce77c2f
fducos <fducos>
2024-09-14 02:52:54 +0200

    New src directory, containing tools based on JSR223 jrunscript

---
+++ src/jacl.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+PREFIX=$PWD/local
+exec jrunscript -cp $PREFIX/lib/tcljava1.5.0/tcljava.jar:$PREFIX/lib/tcljava1.5.0/jacl.jar -l jacl
---
+++ src/tclblend.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+PREFIX=$PWD/local
+LD_LIBRARY_PATH=$PREFIX/lib/tcljava1.5.0:$LD_LIBRARY_PATH exec jrunscript -cp $PREFIX/lib/tcljava1.5.0/tcljava.jar:$PREFIX/lib/tcljava1.5.0/tclblend.jar -l tclblend
