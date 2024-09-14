0e70ae6fd071bdb1defad345605c6dfa03aaf3eb
fducos <fducos>
2024-09-14 04:05:23 +0200

    src/{jacl.sh,tclblend.sh}: now accept arguments

--- src/jacl.sh
+++ src/jacl.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 PREFIX=$PWD/local
-exec jrunscript -cp $PREFIX/lib/tcljava1.5.0/tcljava.jar:$PREFIX/lib/tcljava1.5.0/jacl.jar -l jacl
+exec jrunscript -cp $PREFIX/lib/tcljava1.5.0/tcljava.jar:$PREFIX/lib/tcljava1.5.0/jacl.jar -l jacl "$@"
--- src/tclblend.sh
+++ src/tclblend.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
 PREFIX=$PWD/local
-LD_LIBRARY_PATH=$PREFIX/lib/tcljava1.5.0:$LD_LIBRARY_PATH exec jrunscript -cp $PREFIX/lib/tcljava1.5.0/tcljava.jar:$PREFIX/lib/tcljava1.5.0/tclblend.jar -l tclblend
+LD_LIBRARY_PATH=$PREFIX/lib/tcljava1.5.0:$LD_LIBRARY_PATH exec jrunscript -cp $PREFIX/lib/tcljava1.5.0/tcljava.jar:$PREFIX/lib/tcljava1.5.0/tclblend.jar -l tclblend "$@"
