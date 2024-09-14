660966522ab347fb046b05b20df7b3be389fa24d
fducos <fducos>
2024-09-14 15:59:06 +0200

    tcljava/src/tclblend/tcl/lang/NativeLibraryLoader.java: init() updated

--- tcljava/src/tclblend/tcl/lang/NativeLibraryLoader.java
+++ tcljava/src/tclblend/tcl/lang/NativeLibraryLoader.java
@@ -17,11 +17,12 @@ public class NativeLibraryLoader {
 
     public static void init() {
         /* it is needed to print exception messages before throwing them, because jrunscript (a standard JSR223 tool)
-	 * doesn't display low level exception messages, making troubleshooting difficult
-	 */
-
-	String libName = "tclblend";
-	String libPath = "native/";
+	     * doesn't display low level exception messages, making troubleshooting difficult
+	    */
+	
+        String currentDirectory = System.getProperty("user.dir");
+        String libName = "tclblend";
+        String libPath = "native/";
         try {
             // Determine the appropriate native library path
             String os = System.getProperty("os.name").toLowerCase();
@@ -32,15 +33,15 @@ public class NativeLibraryLoader {
             } else if (os.contains("nix") || os.contains("nux") || os.contains("mac")) {
                 libPath += os.contains("mac") ? "macos/lib" + libName + ".dylib" : "linux/lib" + libName + ".so";
             } else {
-		System.err.println("Unsupported OS: " + os);
+                System.err.println("Unsupported OS: " + os);
                 throw new UnsupportedOperationException("Unsupported OS: " + os);
             }
 
             // Load the library
             InputStream in = NativeLibraryLoader.class.getResourceAsStream("/" + libPath);
             if (in == null) {
-		System.err.println("Native library not found: " + libPath);
-                throw new RuntimeException("Native library not found: " + libPath);
+                System.err.println("Native library not found: " + libPath);
+                throw new RuntimeException("Native library not found: " + libPath + " (current directory: " + currentDirectory + ")");
             }
 
             Path tempFile = Files.createTempFile(libName, getExtension(libPath));
@@ -58,7 +59,7 @@ public class NativeLibraryLoader {
             tempFile.toFile().deleteOnExit();
 
         } catch (Exception e) {
-	    System.err.println("Failed to load native library " + libPath + ": " + e);
+            System.err.println("Failed to load native library " + libPath + ": " + e);
             throw new RuntimeException("Failed to load native library " + libPath + ": " + e);
         }
     }
